0fedecfb4d1cde15eee10e8dd56f6613f2651703
Kojolika <amm982@live.com>
2024-01-19 11:05:20 -0500

    Updated to recognize node and npm versions
    Added mocha and chai and a sample test

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
-const express = require('express');
-const PIXI = require('pixi.js');
-const ejs = require('ejs');
-const app = express();
+import express from 'express';
+import * as PIXI from 'pixi.js';
+import { renderFile } from 'ejs';
 
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
 // set ejs as our view engine
-app.engine('html', ejs.renderFile);
+app.engine('html', renderFile);
 app.set('view engine', 'html');
 app.set('views', './views');
 
 const hostname = '127.0.0.1';
 const port = 3000;
-const directory = __dirname;
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.get('/', (req, res) => {
     res.render('./index', { pixi: PIXI });
 });
 
 app.all('/views/sample.png', (req, res) => {
-    console.log(directory + req.path);
-    res.sendFile(directory + req.path);
+    console.log(__dirname + req.path);
+    res.sendFile(__dirname + req.path);
 });
 
 app.listen(port, () => {
