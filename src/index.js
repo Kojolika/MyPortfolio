7468dae4f76eeff5376f3b7dac94d91d727bcb94
Kojolika <amm982@live.com>
2024-01-24 11:28:45 -0500

    define port in env variable

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 import express from 'express';
 import * as PIXI from 'pixi.js';
-import {renderFile} from 'ejs';
+import { renderFile } from 'ejs';
 
-import {dirname} from 'path';
-import {fileURLToPath} from 'url';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-// test test
+import * as dotEnv from 'dotenv';
+// attach .env variables to process.env
+dotEnv.config();
+
+const port = process.env.PORT;
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
+
 // set ejs as our view engine
 app.engine('html', renderFile);
 app.set('view engine', 'html');
 app.set('views', './views');
 
-const port = 3000;
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
 app.get('/', (req, res) => {
-  res.render('./index', {pixi: PIXI});
+  res.render('./index', { pixi: PIXI });
 });
 
 app.all('/views/sample.png', (req, res) => {
