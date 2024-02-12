44b0c6d01af0d620d2243a8aa09876914fd88bb6
Kojolika <amm982@live.com>
2024-02-12 10:17:38 -0500

    Updated eslint command to check every folder except /dist
    used eslint --fix to fix files

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,48 +1,48 @@
-import HtmlWebpackPlugin from 'html-webpack-plugin';
 import nodeExternals from 'webpack-node-externals';
 import path from 'path';
 
 const dirname = path.resolve();
 
 const clientConfig = (env) => {
-    return {
-        name: "client",
-        entry: path.resolve(dirname, './client/src/pixi/spaceship.js'),
-        output: {
-            path: path.resolve(dirname, './dist'),
-            filename: 'portfolio.bundle.js',
-            //clean: true, // commented out since we are building the server first and then the client
-        },
-        target: 'web',
-        mode: env?.mode == 'production' ? 'production' : 'development',
-        resolve: {
-            modules: ['../node_modules']
-        },
-        externals: [nodeExternals(), 'express'],
-    };
+  return {
+    name: 'client',
+    entry: path.resolve(dirname, './client/src/pixi/spaceship.js'),
+    output: {
+      path: path.resolve(dirname, './dist'),
+      filename: 'portfolio.bundle.js',
+      // commented out since we are building the server first then the client
+      // clean: true,
+    },
+    target: 'web',
+    mode: env?.mode == 'production' ? 'production' : 'development',
+    resolve: {
+      modules: ['../node_modules'],
+    },
+    externals: [nodeExternals(), 'express'],
+  };
 };
 
 const serverConfig = (env) => {
-    return {
-        name: "server",
-        entry: path.resolve(dirname, './server/src/index.js'),
-        output: {
-            path: path.resolve(dirname, './dist'),
-            filename: 'portfolio.node.bundle.js',
-            clean: true, // remove previous files before each use,
-            module: true,
-            chunkFormat: 'module',
-        },
-        target: 'node',
-        mode: env?.mode == 'production' ? 'production' : 'development',
-        resolve: {
-            modules: ['../node_modules']
-        },
-        experiments: {
-            outputModule: true,
-        },
-        externals: ['express'],
-    };
-}
+  return {
+    name: 'server',
+    entry: path.resolve(dirname, './server/src/index.js'),
+    output: {
+      path: path.resolve(dirname, './dist'),
+      filename: 'portfolio.node.bundle.js',
+      clean: true, // remove previous files before each use,
+      module: true,
+      chunkFormat: 'module',
+    },
+    target: 'node',
+    mode: env?.mode == 'production' ? 'production' : 'development',
+    resolve: {
+      modules: ['../node_modules'],
+    },
+    experiments: {
+      outputModule: true,
+    },
+    externals: ['express'],
+  };
+};
 
 export default [clientConfig, serverConfig];
--- test/index.js
+++ test/index.js
@@ -3,9 +3,9 @@ import 'chai/register-should.js';
 import fs from 'fs';
 import path from 'path';
 
-describe('Space Ship Image', function () {
-    const file = path.resolve("./client/public/views/sample.png");
-    it(`should exist in this repository: ${file}`, function () {
-        should.equal(true, fs.existsSync(file));
-    });
+describe('Space Ship Image', function() {
+  const file = path.resolve('./client/public/views/sample.png');
+  it(`should exist in this repository: ${file}`, function() {
+    should.equal(true, fs.existsSync(file));
+  });
 });
