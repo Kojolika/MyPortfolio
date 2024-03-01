1972398f15589adf153a1dad9fa75e98d5438b7d
Kojolika <amm982@live.com>
2024-03-01 11:31:51 -0500

    fix lint

--- server/src/index.js
+++ server/src/index.js
@@ -23,7 +23,7 @@ const envLocals = {
   environment: process.env.NODE_ENV,
   port: process.env.PORT,
   zone: process.env.AWS_ZONE,
-  build: process.env.BUILD
+  build: process.env.BUILD,
 };
 
 app.get('/', (req, res) => {
