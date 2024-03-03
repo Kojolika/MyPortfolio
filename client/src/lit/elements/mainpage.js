3a16f2a6b7f8cf4741ab0b240b05524fbb0f0ca2
Andrew Myshok <36140461+Kojolika@users.noreply.github.com>
2024-03-03 10:41:03 -0500

    testing lit element

---
+++ client/src/lit/elements/mainpage.js
@@ -0,0 +1,9 @@
+import {LitElement, html} from 'lit';
+
+export class MainPage extends LitElement {
+    
+    render(){
+        return html`<main> I am in the main element. </main>`;
+    }
+};
+customElements.define("main-page", MainPage);
