313a6cb53127545dd3ec85a6a555a9d55b4875cc
Kojolika <amm982@live.com>
2024-06-03 12:42:23 -0400

    fix build modes to be changable from dev, added options to show environment on dev builds

--- public/js/lit/elements/env-var-info.js
+++ public/js/lit/elements/env-var-info.js
@@ -1,11 +1,53 @@
-import {LitElement, html} from 'lit';
+import {LitElement, html, css} from 'lit';
+import {standardBorder} from '../styles/standardBorder.js';
+import {icon} from '../styles/icon.js';
 
 /**
  * This class displays all the env variables associated with this build.
  */
 export class EnvVarInfo extends LitElement {
+  /**
+   * Sets initial open value.
+   */
+  constructor() {
+    super();
+
+    this.isOpen = true;
+  }
+
   static properties = {
     envObject: {},
+    isOpen: {type: Boolean},
+  };
+
+  static styles = [icon, standardBorder, css`
+    div {
+      position: fixed;
+      top: 0;
+      right: 0%;
+      background-color: white;
+      display: flex;
+      flex-direction: row;
+    }
+    ul {
+      margin: 0;
+    }
+    button {
+      text-align: center;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding-top: 6px;
+      padding-bottom: 6px;
+      padding-left: 12px;
+      padding-right: 12px;
+      min-width: 32px;
+      min-height: 32px;
+    }
+  `];
+
+  _handleClick = () => {
+    this.isOpen = !this.isOpen;
   };
 
   /**
@@ -18,13 +60,20 @@ export class EnvVarInfo extends LitElement {
     }
 
     return html`
-    <ul>
-    ${Object
-      .keys(this.envObject)
-      .map((e) => {
-        return html`<li> ${e} : ${this.envObject[e]} </li>`;
-      })}
-    </ul>`;
+      <div>
+        <button @click="${this._handleClick}" class="icon-clickable standard-border standard-border-radius">
+          ${this.isOpen?html`>`:html`<`}
+        </button>
+        ${this.isOpen?
+          html`<ul>
+            ${Object
+              .keys(this.envObject)
+              .map((e) => {
+                return html`<li> ${e} : ${this.envObject[e]} </li>`;
+            })}
+          </ul>`:
+          html``}
+      </div>`;
   }
 }
 customElements.define('env-var-info', EnvVarInfo);
--- public/js/lit/styles/icon.js
+++ public/js/lit/styles/icon.js
@@ -3,7 +3,6 @@ import {css} from 'lit';
 export const icon = css`
     .icon, .icon-clickable {
         max-width: 96px;
-        width: 100%;
         border-radius: 10px;
     }
     .icon-clickable:hover
